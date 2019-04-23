b263ba17b08d3fbb83ed4e459a7b8d0e2e3b94f5
ngnawen samuel vermon <ngnawens@gmail.com>
2019-04-23 13:07:12 +0100

    initial commit

---
+++ src/main/java/cm/belrose/gestcontacts/GestcontactsApplication.java
@@ -0,0 +1,13 @@
+package cm.belrose.gestcontacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestcontactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestcontactsApplication.class, args);
+	}
+
+}
